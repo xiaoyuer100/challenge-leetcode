4f099dcc4621124d5a756929e18a7d432445adce
xiaorui <rxiao@hpe.com>
2018-03-30 19:04:28 +0800

    without bit

--- src/main/java/team/six/youth/xiaorui/problem67/Solution.java
+++ src/main/java/team/six/youth/xiaorui/problem67/Solution.java
@@ -25,6 +25,26 @@ public class Solution {
         }
         return String.copyValueOf(result,carry?0:1,carry?result.length:result.length-1);
     }
+
+    // or easier
+    public String addBinary2(String a, String b) {
+        char[] result = new char[Math.max(a.length(),b.length())+1];
+        int res_length = result.length -1;
+        result[0]='0';
+        int a_index = a.length() - 1;
+        int b_index = b.length() - 1;
+        int int_a = 0;
+        int int_b = 0;
+        int carry = 0;
+        while(a_index!=0||b_index>=0){
+            int_a = (a_index == -1?0:a.charAt(a_index--)-'0');
+            int_b = (b_index == -1?0:b.charAt(b_index--)-'0');
+            result[res_length--] = (char)((int_a+int_b+carry)%2 + '0');       //r = !ab!c + a!b!c + !a!bc + abc
+            carry = (int_a+int_b+carry)/2;                                          //c = c = !abc + a!bc + abc + ab!c
+        }
+        result[0] = (char)(carry+'0');
+        return String.copyValueOf(result,carry==0?1:0,result.length-(carry==0?1:0));
+    }
 }
 
 
