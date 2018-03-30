b315b807780ed1105997c3754515ee204b2c3756
rayshaw001 <396301947@163.com>
2018-03-30 09:19:33 +0800

    xaiorui

---
+++ src/main/java/team/six/youth/problem30/xiaorui/Solution.java
@@ -0,0 +1,73 @@
+package team.six.youth.problem30.xiaorui;
+
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        ArrayList<Integer> results = new ArrayList<>();
+        int index[] = new int[words.length];
+        int max[] = new int[words.length];
+        int matched = 0;
+        int result = 0;
+        int gap = words[0].length();
+        int s_size = s.length();
+
+        for (int i=0;i<words.length;i++){
+            max[contains(words,words[i],0)]++;
+        }
+
+        for (int i=0;i+gap<=s_size;){
+            int test = contains(words,s,i);
+            if(test==-1){
+                for (int j=0;j<index.length;j++) {
+                    index[j] = 0;
+                }
+                if(matched==0) {
+                    result ++;
+                    i=result;
+                }
+                matched = 0;
+            } else {
+                if(index[test]==max[test]){
+                    for (int j=0;j<index.length;j++) {
+                        index[j] = 0;
+                    }
+                    matched = 0;
+                    result ++;
+                    i = result;
+                } else {
+                    index[test] ++;
+                    matched ++;
+                    i += gap;
+                    if(matched==words.length){
+                        results.add(result);
+                        result += 1;
+                        for (int j=0;j<index.length;j++) {
+                            index[j] = 0;
+                        }
+                        matched = 0;
+                        i = result;
+                    }
+                }
+            }
+            
+        }
+        return results;
+    }
+
+    private final int contains(String[]words,String s,int start){
+        for (int i=0;i<words.length;i++){
+            if(equals(words[i],s,start)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private final boolean equals(String word,String s,int start){
+        for(int i=0;i<word.length();i++){
+            if(word.charAt(i)!=s.charAt(i+start)){
+                return Boolean.FALSE;
+            }
+        }
+        return Boolean.TRUE;
+    }
+}
