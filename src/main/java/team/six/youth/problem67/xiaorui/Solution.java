d5da2619ddc39cc596d429838110daa744712b12
xiaorui <rxiao@hpe.com>
2018-03-30 13:11:42 +0800

    prettify

--- src/main/java/team/six/youth/problem67/xiaorui/Solution.java
+++ src/main/java/team/six/youth/problem67/xiaorui/Solution.java
@@ -1,26 +1,28 @@
 package team.six.youth.problem67.xiaorui;
 
+import java.util.Stack;
+
 public class Solution {
     public static void main (String args[]){
-        System.out.print(new Solution().addBinary("11","101"));
+        System.out.print(new Solution().addBinary("1","0"));
     }
 
     public String addBinary(String a, String b) {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         int a_index = a.length() - 1;
         int b_index = b.length() - 1;
         int int_a;
         int int_b;
         int carry = 0;
-
+        Stack<Integer> stack = new Stack<>();
         while(a_index>=0||b_index>=0){
             int_a = a_index >= 0?a.charAt(a_index--)-'0':0;
             int_b = b_index >= 0?b.charAt(b_index--)-'0':0;
-            sb.append((~int_a) & int_b&(~carry) | int_a&(~int_b)&(~carry) | (~int_a)&(~int_b)&carry | int_a&int_b&carry);     //r = !ab!c + a!b!c + !a!bc + abc
-            carry = (~int_a)&int_b&carry | int_a&(~int_b)&carry | int_a&int_b&carry | int_a&int_b&(~carry);                //c = c = !abc + a!bc + abc + ab!c
+            sb.insert(0,(int_a^int_b)&(~carry) | (~(int_a^int_b))&carry);       //r = !ab!c + a!b!c + !a!bc + abc
+            carry = int_a&int_b | (int_a^int_b)&carry;                                  //c = c = !abc + a!bc + abc + ab!c
         }
-        sb.append(carry==0?"":carry);
-        return sb.reverse().toString();
+        sb.insert(0,carry==0?"":carry);
+        return sb.toString();
     }
 }
 
