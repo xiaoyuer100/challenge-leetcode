002c8d3c3738679dac0030f4dbf1c1a3b7ca74cb
guiqiang <guiqiang@playcrab.com>
2018-03-31 18:25:09 +0800

    update

--- src/main/java/team/six/youth/guiqiang/_3_31_p31_NextPermutation.java
+++ src/main/java/team/six/youth/guiqiang/_3_31_p31_NextPermutation.java
@@ -1,7 +1,20 @@
 package team.six.youth.guiqiang;
 
 import java.util.Arrays;
+/**
+ * 
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
 
+If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+
+The replacement must be in-place, do not allocate extra memory.
+
+Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+1,2,3 → 1,3,2
+3,2,1 → 1,2,3
+1,1,5 → 1,5,1
+ *
+ */
 public class _3_31_p31_NextPermutation {
 	public void nextPermutation(int[] nums) {
 		boolean flag = false;
