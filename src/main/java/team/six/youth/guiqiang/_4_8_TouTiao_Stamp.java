4c5f262d41728ccb8cf49ea9633463d5f8aef670
guiqiang <guiqiang@playcrab.com>
2018-04-09 19:49:41 +0800

    p10 guiqiang

--- src/main/java/team/six/youth/guiqiang/_4_8_TouTiao_Stamp.java
+++ src/main/java/team/six/youth/guiqiang/_4_8_TouTiao_Stamp.java
@@ -7,7 +7,7 @@ import java.util.Map;
    某人有四张3分的邮票和三张5分的邮票，用这些邮票中的一张或若干张可以得到多少种不同的邮资？
  *
  */
-public class _4_5_TouTiao_Stamp {
+public class _4_8_TouTiao_Stamp {
 	public int CountPostage(int num3, int num5){
 		Map<Integer, Integer> countMap = new HashMap<Integer, Integer>();
 		for (int i = 0; i <= num3; i++) {
@@ -20,7 +20,7 @@ public class _4_5_TouTiao_Stamp {
 	}
 	
 	public static void main(String[] args) {
-		_4_5_TouTiao_Stamp solution = new _4_5_TouTiao_Stamp();
+		_4_8_TouTiao_Stamp solution = new _4_8_TouTiao_Stamp();
 		int result = solution.CountPostage(4, 3);
 		System.out.println(result);
 	}
