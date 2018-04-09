ac181d35665f23721f9b0b5e654ac5e8c819e315
xiaorui <rxiao@hpe.com>
2018-04-09 13:42:57 +0800

    correct issue

--- src/main/java/team/six/youth/xiaorui/addBinary67.java
+++ src/main/java/team/six/youth/xiaorui/addBinary67.java
@@ -1,6 +1,12 @@
 package team.six.youth.xiaorui;
 
 public class addBinary67 {
+
+    public static void main (String args[]){
+        System.out.print(new addBinary67().new Solution().addBinary("1","0"));
+    }
+
+
     class Solution {
 
         public String addBinary(String a, String b) {
